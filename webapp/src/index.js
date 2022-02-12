fb53082f147760a17262dbf98e7e28bf6efde752
Martin Beltran <uo276244@uniovi.es>
2022-02-12 19:15:09 +0100

    Branch with basic React Project

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/**import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -15,3 +15,9 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+**/
+import React from "react";
+import ReactDOM from "react-dom";
+
+const element = <h1>Hello World!</h1>;
+ReactDOM.render(element,document.getElementById("root"));
