c5c8bb75062f999d20e409046d23378670f97ebf
EnzoBarbon <enzobl2000@gmail.com>
2022-03-02 20:43:39 +0100

    UO270249 -- Set up firebase and firestore. Change the calls to the node.js api to the firestore.

--- webapp/src/api/api.ts
+++ webapp/src/api/api.ts
@@ -1,21 +1,18 @@
-import {User} from '../shared/shareddtypes';
+import { addDoc, collection, getDocs } from "firebase/firestore";
+import { db } from "../utils/firebase";
+import { User } from "./model/user";
 
-export async function addUser(user:User):Promise<boolean>{
-    const apiEndPoint= process.env.REACT_APP_API_URI || 'http://localhost:5000/api'
-    let response = await fetch(apiEndPoint+'/users/add', {
-        method: 'POST',
-        headers: {'Content-Type':'application/json'},
-        body: JSON.stringify({'name':user.name, 'email':user.email})
-      });
-    if (response.status===200)
-      return true;
-    else
-      return false;
+const userCollection = collection(db, "user");
+
+export async function addUser(user: User): Promise<any> {
+  return addDoc(userCollection, {
+    created: Date.now(),
+    ...user,
+  });
 }
 
-export async function getUsers():Promise<User[]>{
-    const apiEndPoint= process.env.REACT_APP_API_URI || 'http://localhost:5000/api'
-    let response = await fetch(apiEndPoint+'/users/list');
-    //The objects returned by the api are directly convertible to User objects
-    return response.json()
+export async function getUsers(): Promise<any> {
+  return getDocs(userCollection).then((docs) =>
+    docs.docs.map((doc) => doc.data() as User)
+  );
 }
