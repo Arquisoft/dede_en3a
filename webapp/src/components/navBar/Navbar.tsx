dd08099a05dd3391dbc3d32ab54bcd143b02d556
Martin Beltran <uo276244@uniovi.es>
2022-03-02 17:14:58 +0100

    Initial creation of navbar with importing errors

---
+++ webapp/src/components/navBar/Navbar.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+
+import {Container,LogoContainer,Menu,MenuItem,MenuItemLink,Wrapper} from "./Navbar.elements";
+
+const Navbar = () => {
+
+    return(
+
+        <Container>
+
+            <LogoContainer>
+
+            </LogoContainer>
+
+            <Menu>
+
+                <MenuItem>
+                    <MenuItemLink>
+
+                    </MenuItemLink>
+                </MenuItem>
+
+                <MenuItem>
+                    <MenuItemLink>
+
+                    </MenuItemLink>
+                </MenuItem>
+
+
+                <MenuItem>
+                    <MenuItemLink>
+
+                    </MenuItemLink>
+                </MenuItem>
+
+
+                <MenuItem>
+                    <MenuItemLink>
+
+                    </MenuItemLink>
+                </MenuItem>
+
+
+            </Menu>
+
+
+        </Container>
+
+    );
+
+}
+
+export default Navbar;
