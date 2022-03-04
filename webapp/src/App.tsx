8ffc9008c6bfd9227e126abe7841be4e9e3c6ee9
Josué Argüelles <UO277912@uniovi.es>
2022-03-04 13:16:44 +0100

    Shopping Cart

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -7,11 +7,77 @@ import Welcome from './components/Welcome';
 import UserList from './components/UserList';
 import  {getUsers} from './api/api';
 import {User} from './shared/shareddtypes';
-import './App.css';
+import './App.css'
+
+//Shopping Cart
+import { useQuery } from 'react-query';
+// Components
+import Item from './components/Item/Item';
+import Cart from './components/Cart/Cart';
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+// Styles
+import { Wrapper, StyledButton } from './App.styles';
+
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> =>
+    await (await fetch('https://fakestoreapi.com/products')).json();
 
 function App(): JSX.Element {
 
   const [users,setUsers] = useState<User[]>([]);
+    const [cartOpen, setCartOpen] = useState(false);
+    const [cartItems, setCartItems] = useState([] as CartItemType[]);
+    const { data, isLoading, error } = useQuery<CartItemType[]>(
+        'products',
+        getProducts
+    );
+    console.log(data);
+
+    const getTotalItems = (items: CartItemType[]) =>
+        items.reduce((ack: number, item) => ack + item.amount, 0);
+
+    const handleAddToCart = (clickedItem: CartItemType) => {
+        setCartItems(prev => {
+            // 1. Is the item already added in the cart?
+            const isItemInCart = prev.find(item => item.id === clickedItem.id);
+
+            if (isItemInCart) {
+                return prev.map(item =>
+                    item.id === clickedItem.id
+                        ? { ...item, amount: item.amount + 1 }
+                        : item
+                );
+            }
+            // First time the item is added
+            return [...prev, { ...clickedItem, amount: 1 }];
+        });
+    };
+
+    const handleRemoveFromCart = (id: number) => {
+        setCartItems(prev =>
+            prev.reduce((ack, item) => {
+                if (item.id === id) {
+                    if (item.amount === 1) return ack;
+                    return [...ack, { ...item, amount: item.amount - 1 }];
+                } else {
+                    return [...ack, item];
+                }
+            }, [] as CartItemType[])
+        );
+    };
 
   const refreshUserList = async () => {
     setUsers(await getUsers());
@@ -21,16 +87,33 @@ function App(): JSX.Element {
     refreshUserList();
   },[]);
 
+    if (isLoading) return <LinearProgress />;
+    if (error) return <div>Something went wrong ...</div>;
+
   return (
-    <>
-      <Container maxWidth="sm">
-        <Welcome message="ASW students"/>
-        <Box component="div" sx={{ py: 2}}>This is a basic example of a React application using Typescript. You can add your email to the list filling the form below.</Box>
-        <EmailForm OnUserListChange={refreshUserList}/>        
-        <UserList users={users}/>
-        <Link href="https://github.com/pglez82/asw2122_0">Source code</Link>
-      </Container>
-    </>
+
+      <Wrapper>
+          <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
+              <Cart
+                  cartItems={cartItems}
+                  addToCart={handleAddToCart}
+                  removeFromCart={handleRemoveFromCart}
+              />
+          </Drawer>
+          <StyledButton onClick={() => setCartOpen(true)}>
+              <Badge badgeContent={getTotalItems(cartItems)} color='error'>
+                  <AddShoppingCartIcon />
+              </Badge>
+          </StyledButton>
+          <Grid container spacing={3}>
+              {data?.map(item => (
+                  <Grid item key={item.id} xs={12} sm={4}>
+                      <Item item={item} handleAddToCart={handleAddToCart} />
+                  </Grid>
+              ))}
+          </Grid>
+      </Wrapper>
+
   );
 }
 
