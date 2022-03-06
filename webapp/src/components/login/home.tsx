486754e1b7a7c8175bdf85f599a4bdf90d375848
Martin Beltran <uo276244@uniovi.es>
2022-03-06 21:17:15 +0100

    Initial components with ROUTES

--- webapp/src/components/login/home.tsx
+++ webapp/src/components/login/home.tsx
@@ -1,17 +1,6 @@
-import React from 'react'
 
-export const Home = () => {
-    return (
-        <div className='card'>
-            <div className='card-header' >
-                <img src='https://cdn0.iconfinder.com/data/icons/set-ui-app-android/32/8-512.png' alt='user' className='user-photo' />
-            </div>
-            <div className='card-body'>
-                <h1>Welcome</h1>
-                <p>user@gmail.com</p>
-                <button className='logout-button' >Log Out</button>
-            </div>
+import {Routes,Route} from 'react-router-dom';
 
-        </div>
-    )
+export function Home(){
+return <div>home</div>
 }
--- webapp/src/components/login/login.tsx
+++ webapp/src/components/login/login.tsx
@@ -1,23 +1,5 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import {Routes,Route} from 'react-router-dom';
 
-export const Login = () => {
-    const [error, setError] = useState(null);
-    return (
-        <div className='card'>
-            <div className='card-header' >
-                {error && <p className='error' >{{ error }}</p>}
-                <h1>Log In</h1>
-            </div>
-            <div className='card-body'>
-                <form>
-                    <input type='email' placeholder='Email' />
-                    <input type='password' placeholder='Password' />
-                    <input type='submit' value='Log In' />
-                </form>
-                <p>Don't have an account? <Link to='/signup'>Sign Up</Link> </p>
-            </div>
-
-        </div>
-    )
+export function Login(){
+    return <div>login</div>
 }
