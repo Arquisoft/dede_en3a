df11a2cb68ea81d51c58d38c772bba612554009b
Martin Beltran <uo276244@uniovi.es>
2022-03-06 20:00:54 +0100

    Merge with Master changes

--- webapp/src/components/navBar/Navbar.tsx
+++ webapp/src/components/navBar/Navbar.tsx
@@ -1,54 +1,82 @@
-import React from "react";
+import React,{useState} from "react";
 
 
-import {Container,LogoContainer,Menu,MenuItem,MenuItemLink,Wrapper} from "./Navbar.elements";
+import {Container,Wrapper,LogoContainer,Menu,MenuItem,MenuItemLink,MobileIcon} from "./Navbar.elements";
+import { SiAiohttp } from "react-icons/si";
+import { FaBars} from 'react-icons/fa'
+import {IconContext} from "react-icons";
+import GoogleLogin from 'react-google-login';
+import ReactDOM from "react-dom";
+import App from "../../App";
 
 const Navbar = () => {
+
+    const[showMobileMenu, setShowMobileMenu] = useState(false);
 
     return(
 
         <Container>
-
-            <LogoContainer>
-
-            </LogoContainer>
-
-            <Menu>
-
-                <MenuItem>
-                    <MenuItemLink>
-
-                    </MenuItemLink>
-                </MenuItem>
-
-                <MenuItem>
-                    <MenuItemLink>
-
-                    </MenuItemLink>
-                </MenuItem>
+            <Wrapper>
+                <IconContext.Provider value={{style:{fontSize:"2em"}}}>
 
 
-                <MenuItem>
-                    <MenuItemLink>
+                    <LogoContainer>
+                        <SiAiohttp></SiAiohttp>
+                        <p>De</p><p>De</p>
+                    </LogoContainer>
+                    <MobileIcon onClick={
 
-                    </MenuItemLink>
-                </MenuItem>
+                        ()=>setShowMobileMenu(!showMobileMenu)}>
+
+                        <FaBars/>
+                    </MobileIcon>
+
+                    <Menu theme={showMobileMenu}>
+
+                        <MenuItem>
+                            <MenuItemLink>
+                                <GoogleLogin
+                                    clientId="1007178029696-bao0ucmangfj9f5of5dkbqk7mmiifqqq.apps.googleusercontent.com"
+                                    buttonText="Login"
+                                    onSuccess={responseGoogle}
+                                    onFailure={responseGoogle}
+                                    cookiePolicy={'single_host_origin'}/>
+                            </MenuItemLink>
+                        </MenuItem>
 
 
-                <MenuItem>
-                    <MenuItemLink>
 
-                    </MenuItemLink>
-                </MenuItem>
+                        <MenuItem>
+                            <MenuItemLink>
+                                ORDERS
+                            </MenuItemLink>
+                        </MenuItem>
+
+                        <MenuItem>
+                            <MenuItemLink>
+                                ORDERS
+                            </MenuItemLink>
+                        </MenuItem>
 
 
-            </Menu>
+                        <MenuItem>
+                            <MenuItemLink>
+                                SHOPPING CART
+                            </MenuItemLink>
+                        </MenuItem>
 
 
+                    </Menu>
+                </IconContext.Provider>
+            </Wrapper>
         </Container>
 
     );
 
+}
+
+const responseGoogle = (response:any) => {
+    console.log(response);
 }
 
 export default Navbar;
