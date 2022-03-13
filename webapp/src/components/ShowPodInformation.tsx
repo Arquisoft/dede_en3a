36ee423e01bbd9ecf8d7e345bfcd4b97f91d2e44
carlosgarriga12 <carlosgarriga12@gmail.com>
2022-03-13 19:36:14 +0100

    Updated part of the documentation. The diagrams still need to be updated

--- webapp/src/components/ShowPodInformation.tsx
+++ webapp/src/components/ShowPodInformation.tsx
@@ -12,23 +12,21 @@ type PODProps = {
     webID: string;
 };
 
-async function retrievePODInfo(webID: string): Promise<string> {
-    let profileDocumentURI = webID.split("#")[0]; // we are just interested in the cardç
-    let myDataset = await getSolidDataset(profileDocumentURI); // obtain the dataset from the URI
-    let profile = getThing(myDataset, webID); // we obtain the thing we are looking for from the dataset
+async function retrievePODAdress(webID: string): Promise<string> {
+    let profileDocumentURI = webID.split("#")[0];
+    let myDataset = await getSolidDataset(profileDocumentURI);
+    let profile = getThing(myDataset, webID);
     let name = getStringNoLocale(profile as Thing, VCARD.street_address) as string;
-    // we obtain the property we are looking for and return it
 
 
     return name
-
 }
 
 function ShowPodInformation(props: PODProps): JSX.Element {
 
     const [name, setName] = React.useState("");
 
-    const getPODName = async () => setName(await retrievePODInfo(props.webID));
+    const getPODName = async () => setName(await retrievePODAdress(props.webID));
 
     useEffect(() => {
         getPODName();
