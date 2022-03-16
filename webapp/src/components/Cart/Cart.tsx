94205ca3ac2d6236d02ddb236ce35549a79ede3c
Martin Beltran <uo276244@uniovi.es>
2022-03-16 21:38:51 +0100

    Merged with develop

--- webapp/src/components/Cart/Cart.tsx
+++ webapp/src/components/Cart/Cart.tsx
@@ -8,11 +8,16 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import productCartItem from "./ProductCartItem/ProductCartItem";
 import { useAuth } from "../../context/AuthContext";
+import {getFunctions, httpsCallable} from "firebase/functions";
 
 type CartProps = {};
 
 function Cart(props: CartProps): JSX.Element {
   const navigate = useNavigate();
+
+
+
+
 
   const calculateTotal = (items: CartItem[]) => {
     console.log("recalculate");
@@ -25,6 +30,29 @@ function Cart(props: CartProps): JSX.Element {
   let itemList: JSX.Element[] = [];
 
   const products: CartItem[] = useSelector((state: DedeStore) => state.cart);
+  const currentUser = useAuth().getCurrentUser();
+
+
+    const  add = async () => {
+
+
+        const sendOrder = httpsCallable(getFunctions(), 'sendOrder');
+
+        return await sendOrder({
+            items:products,
+            user:currentUser?.email,
+            address:"Casa de " + currentUser?.email
+        })
+            .then(( ) => {
+                console.log("Your order has been processed.");
+            }).catch(()=>{
+
+                console.log("Sorry, we are suffering technical problems, try again...");
+
+            });
+
+    }
+
 
   products.forEach((cartItem) => {
     if (cartItem.amount > 0) {
@@ -32,14 +60,20 @@ function Cart(props: CartProps): JSX.Element {
     }
   });
 
-  const currentUser = useAuth().getCurrentUser();
+
 
   const userRegistered = () => {
     return currentUser !== null;
   };
 
+
   const buy = () => {
     if (userRegistered()) {
+
+       add().then(()=>{
+
+       });
+
     } else {
       navigate("/login");
     }
