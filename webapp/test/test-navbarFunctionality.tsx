de58c222dd9ca1c992b277d829c7250c090b8228
Josué Argüelles <UO277912@uniovi.es>
2022-03-27 16:34:29 +0200

    Navbar test shop

--- webapp/test/test-navbarFunctionality.tsx
+++ webapp/test/test-navbarFunctionality.tsx
@@ -2,13 +2,20 @@ import {fireEvent, render} from "@testing-library/react";
 import * as React from "react";
 import TopMenu from "../src/components/menu/TopMenu";
 
-
-
 test ( 'Navbar home test', async () => {
 
     const {getByText} = render( <TopMenu></TopMenu> );
     const homeButton = getByText("Home");
     fireEvent.click(homeButton);
     expect(getByText("Dede")).toBeInTheDocument();
+
+} );
+
+test ( 'Navbar shop test', async () => {
+
+    const {getByText} = render( <TopMenu></TopMenu> );
+    const shopButton = getByText("Shop");
+    fireEvent.click(shopButton);
+    expect(getByText("Shop")).toBeInTheDocument();
 
 } );
