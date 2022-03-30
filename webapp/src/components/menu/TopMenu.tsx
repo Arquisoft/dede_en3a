e4b047062c81ff2f3818f2c13709d11471965cc0
EnzoBarbon <enzobl2000@gmail.com>
2022-03-30 20:24:43 +0200

    Added responsive menu bar

--- webapp/src/components/menu/TopMenu.tsx
+++ webapp/src/components/menu/TopMenu.tsx
@@ -9,6 +9,9 @@ import { CartItem } from "../../redux/models/CartItem";
 type TopMenuProps = {};
 function TopMenu(): JSX.Element {
   const [wobble, setWobble] = useState(String);
+  const [expandableMenuClass, setExpandableMenuClass] = useState(
+    styles.expandablemenu
+  );
   const navigate = useNavigate();
 
   const url = window.location.href.split("/");
@@ -16,6 +19,15 @@ function TopMenu(): JSX.Element {
   const path = url[1];
 
   const cart = useSelector((state: DedeStore) => state.cart);
+
+  const expandMenu = () => {
+    console.log(expandableMenuClass);
+    setExpandableMenuClass(
+      expandableMenuClass === styles.expandablemenu
+        ? styles.expandablemenu + " " + styles.showmenu
+        : styles.expandablemenu
+    );
+  };
 
   const endWobble = () => {
     console.log("endWobble");
@@ -27,6 +39,7 @@ function TopMenu(): JSX.Element {
   if (path === "home") homeClass = styles.menuitem + " " + styles.selected;
   return (
     <>
+      {/* Normal menu */}
       <div className={styles.menucontainer}>
         <div className={styles.menu}>
           <img src={logo} className={styles.logo} alt="logo" />
@@ -34,12 +47,24 @@ function TopMenu(): JSX.Element {
             <div className={homeClass} onClick={() => navigate("/home")}>
               Home
             </div>
-            <div className={styles.menuitem} onClick={ () => navigate("/shop") }>Shop</div>
+            <div className={styles.menuitem} onClick={() => navigate("/shop")}>
+              Shop
+            </div>
             <a href={"https://arquisoft.github.io/dede_en3a/"}>
               <div className={styles.menuitem}>About us</div>
             </a>
-            <div className={styles.menuitem} onClick={ () => navigate("/contact") }>Contact</div>
-            <div className={styles.menuitem} onClick={ () => navigate("/orders") }>Orders</div>
+            <div
+              className={styles.menuitem}
+              onClick={() => navigate("/contact")}
+            >
+              Contact
+            </div>
+            <div
+              className={styles.menuitem}
+              onClick={() => navigate("/orders")}
+            >
+              Orders
+            </div>
             <div className={styles.cartcontainer} onAnimationEnd={endWobble}>
               <span
                 className={"material-icons " + styles.loginicon}
@@ -56,6 +81,83 @@ function TopMenu(): JSX.Element {
           >
             account_circle
           </span>
+        </div>
+      </div>
+      {/* Mobile menu */}
+      <div className={styles.mobilemenucontainer}>
+        <div className={styles.menu}>
+          <span
+            className={
+              "material-icons " + styles.loginicon + " " + styles.menuicon
+            }
+            onClick={() => expandMenu()}
+          >
+            menu
+          </span>
+          <img src={logo} className={styles.logo} alt="logo" />
+          <div className={styles.cartcontainer} onAnimationEnd={endWobble}>
+            <span
+              className={"material-icons " + styles.loginicon}
+              onClick={() => navigate("/cart")}
+            >
+              shopping_cart
+            </span>
+            <div className={styles.cartcounter}>{cart.length}</div>
+          </div>
+        </div>
+      </div>
+
+      {/* Expandable mobile left menu */}
+      <div className={expandableMenuClass}>
+        <div className={styles.links}>
+          <div
+            className={homeClass}
+            onClick={() => {
+              navigate("/home");
+              expandMenu();
+            }}
+          >
+            Home
+          </div>
+          <div
+            className={styles.menuitem}
+            onClick={() => {
+              navigate("/shop");
+              expandMenu();
+            }}
+          >
+            Shop
+          </div>
+          <div>
+            <a href={"https://arquisoft.github.io/dede_en3a/"}>
+              <div
+                className={styles.menuitem}
+                onClick={() => {
+                  expandMenu();
+                }}
+              >
+                About us
+              </div>
+            </a>
+          </div>
+          <div
+            className={styles.menuitem}
+            onClick={() => {
+              navigate("/contact");
+              expandMenu();
+            }}
+          >
+            Contact
+          </div>
+          <div
+            className={styles.menuitem}
+            onClick={() => {
+              navigate("/orders");
+              expandMenu();
+            }}
+          >
+            Orders
+          </div>
         </div>
       </div>
     </>
