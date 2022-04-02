a46f0a2e0f7affa6481f2b6ceb4402b4686c6b41
Josué Argüelles <UO277912@uniovi.es>
2022-04-02 16:42:41 +0200

    Home page tests

---
+++ webapp/src/renderHomePage.test.tsx
@@ -0,0 +1,30 @@
+import {render} from "@testing-library/react";
+import * as React from "react";
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import {store} from "./redux/store";
+import MainPage from "./components/pages/mainPage/MainPage";
+
+test("the Dede title is rendered", async () => {
+
+    const {getByText} = render( <BrowserRouter><Provider store={store}> <MainPage></MainPage></Provider></BrowserRouter> );
+    const titleDede = getByText("Dede");
+    expect(titleDede).toBeInTheDocument();
+
+});
+
+test("the subtitle is rendered", async () => {
+
+    const {getByText} = render( <BrowserRouter><Provider store={store}> <MainPage></MainPage></Provider></BrowserRouter> );
+    const subtitle = getByText("A decentralized ecommerce website.");
+    expect(subtitle).toBeInTheDocument();
+
+});
+
+test("the main text of the page is rendered", async () => {
+
+    const {getByText} = render( <BrowserRouter><Provider store={store}> <MainPage></MainPage></Provider></BrowserRouter> );
+    const text = getByText("Innovational use of Solid Pods in order to make our deliveries more private.");
+    expect(text).toBeInTheDocument();
+
+});
