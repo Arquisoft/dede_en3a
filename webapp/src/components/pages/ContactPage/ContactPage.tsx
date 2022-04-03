445abea52fac4798e6f4fde37005c9b3e8765381
Josué Argüelles <UO277912@uniovi.es>
2022-04-03 23:41:51 +0200

    Contact page tests

--- webapp/src/components/pages/ContactPage/ContactPage.tsx
+++ webapp/src/components/pages/ContactPage/ContactPage.tsx
@@ -7,9 +7,9 @@ function ContactPage():JSX.Element{
             <TopMenu></TopMenu>
             <div className="header-container">
                 <div className="header">
-                    <div className="title">Contact</div>
+                    <div title={"contact"} className="title">Contact</div>
                     <div className="subtitle">
-                        <img src={"https://logodix.com/logo/1288191.png"} width={"50"} height={"50"}/>
+                        <img title={"contactImage"} src={"https://logodix.com/logo/1288191.png"} width={"50"} height={"50"}/>
                         <p>Email: dedeen3a@gmail.com</p>
                     </div>
                 </div>
---
+++ webapp/src/test/renderContactPage.test.tsx
@@ -0,0 +1,30 @@
+import {render} from "@testing-library/react";
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import {store} from "../redux/store";
+import * as React from "react";
+import ContactPage from "../components/pages/ContactPage/ContactPage";
+
+test("the contact title is rendered", async () => {
+
+    const {getByTitle} = render( <BrowserRouter><Provider store={store}> <ContactPage></ContactPage> </Provider></BrowserRouter> );
+    const titleContact = getByTitle("contact");
+    expect(titleContact).toBeInTheDocument();
+
+});
+
+test("the gmail logo is rendered", async () => {
+
+    const {getByTitle} = render( <BrowserRouter><Provider store={store}> <ContactPage></ContactPage> </Provider></BrowserRouter> );
+    const contactImage = getByTitle("contactImage");
+    expect(contactImage).toBeInTheDocument();
+
+});
+
+test("the email text is rendered", async () => {
+
+    const {getByText} = render( <BrowserRouter><Provider store={store}> <ContactPage></ContactPage> </Provider></BrowserRouter> );
+    const email = getByText("Email: dedeen3a@gmail.com");
+    expect(email).toBeInTheDocument();
+
+});
