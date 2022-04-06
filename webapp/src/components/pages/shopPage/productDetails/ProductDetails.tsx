396a177e65ad28742853b4d8606a2bf5876f7bd3
Pablo García Fernández <uo276824@uniovi.es>
2022-04-06 10:50:37 +0200

    Added read comments from db

--- webapp/src/components/pages/shopPage/productDetails/ProductDetails.tsx
+++ webapp/src/components/pages/shopPage/productDetails/ProductDetails.tsx
@@ -20,7 +20,25 @@ function ProductDetails(): JSX.Element {
         refreshProductList();
     }, []);
 
+    function loadComments() {
+        products.forEach((product) => {
+            product.comments?.forEach((comment) => {
+                commentList.push(
+                    <div className={styles.wrapperWholeComment}>
+                        <div className={styles.commentWrapper}>
+                            <img className={styles.userImage} src={comment.userImage} ></img>
+                            <div className={styles.username}>{comment.author}</div>
+                        </div>
+                        <Rating name="read-only" value={comment.rating} readOnly size={"small"}/>
+                        <div className={styles.text}>{comment.message}</div>
+                    </div>
+                );
+            });
+        });
+    }
+
     let productList: JSX.Element[] = [];
+    let commentList: JSX.Element[] = [];
 
     if (products != null || products != undefined) {
         products.forEach((product) => {
@@ -34,7 +52,7 @@ function ProductDetails(): JSX.Element {
                         <div className={styles.subtitle}>{"Price: " + product.price + " $"}</div>
                         <div className={styles.subtitle}>Description: </div>
                         <div className={styles.text}>{product.description}</div>
-                        <div className={styles.subtitle}>RATING:</div>
+                        <div className={styles.subtitle}>Rating:</div>
                         <Rating name="half-rating" defaultValue={2.5} precision={0.5} size="large"/>
                     </div>
                 </div>
@@ -47,9 +65,13 @@ function ProductDetails(): JSX.Element {
             <TopMenu></TopMenu>
             <>{productList}</>
             <div className={styles.subheader}>
-                <div className={styles.subsubtitle}>ADD COMMENT</div>
+                <div className={styles.subsubtitle}>Add a Review:</div>
                 <input className={styles.inputComment} id= "input-form" type={"text"}></input><br/><br/>
-                <div className={styles.subsubtitle}>COMENTS:</div>
+                <div className={styles.subsubtitle}>Reviews:</div>
+                {
+                    loadComments()
+                }
+                <>{commentList}</>
             </div>
         </>
     );
