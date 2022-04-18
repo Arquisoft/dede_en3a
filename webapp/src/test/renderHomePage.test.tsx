c225e0c7cba0ad7c534202c298f7cf205bc95cb3
Josué Argüelles <UO277912@uniovi.es>
2022-04-18 19:53:36 +0200

    New tests over the home page

--- webapp/src/test/renderHomePage.test.tsx
+++ webapp/src/test/renderHomePage.test.tsx
@@ -13,10 +13,11 @@ test("the Dede title is rendered", async () => {
 
 });
 
+
 test("the subtitle is rendered", async () => {
 
     const {getByText} = render( <BrowserRouter><Provider store={store}> <MainPage></MainPage></Provider></BrowserRouter> );
-    const subtitle = getByText("A decentralized ecommerce website.");
+    const subtitle = getByText(", a decentralized ecommerce website");
     expect(subtitle).toBeInTheDocument();
 
 });
@@ -25,6 +26,76 @@ test("the main text of the page is rendered", async () => {
 
     const {getByText} = render( <BrowserRouter><Provider store={store}> <MainPage></MainPage></Provider></BrowserRouter> );
     const text = getByText("Innovational use of Solid Pods in order to make our deliveries more private.");
+    expect(text).toBeInTheDocument();
+
+});
+
+test("See more information", async () => {
+
+    const {getByText} = render( <BrowserRouter><Provider store={store}> <MainPage></MainPage></Provider></BrowserRouter> );
+    const text = getByText("See more");
+    expect(text).toBeInTheDocument();
+
+});
+
+test("Information block title is rendered", async () => {
+
+    const {getByText} = render( <BrowserRouter><Provider store={store}> <MainPage></MainPage></Provider></BrowserRouter> );
+    const text = getByText("How does it work?");
+    expect(text).toBeInTheDocument();
+
+});
+
+test("Choose product - title", async () => {
+
+    const {getByText} = render( <BrowserRouter><Provider store={store}> <MainPage></MainPage></Provider></BrowserRouter> );
+    const text = getByText("First,");
+    expect(text).toBeInTheDocument();
+
+});
+
+
+test("Choose product - body", async () => {
+
+    const {getByText} = render( <BrowserRouter><Provider store={store}> <MainPage></MainPage></Provider></BrowserRouter> );
+    const text = getByText("Choose between our wide array of available products, from" +
+        " medical items to technological gadgets. Once you have found your" +
+        " desired products, proceed to checkout.");
+    expect(text).toBeInTheDocument();
+
+});
+
+test("Use your POD - title", async () => {
+
+    const {getByText} = render( <BrowserRouter><Provider store={store}> <MainPage></MainPage></Provider></BrowserRouter> );
+    const text = getByText("Then,");
+    expect(text).toBeInTheDocument();
+
+});
+
+test("Use your POD - body", async () => {
+
+    const {getByText} = render( <BrowserRouter><Provider store={store}> <MainPage></MainPage></Provider></BrowserRouter> );
+    const text = getByText("In order to proceed with the checkout, you will have to" +
+        " introduce your solid POD WebId. If you dont have one you can" +
+        " create one here?. We support solid community pods???");
+    expect(text).toBeInTheDocument();
+
+});
+
+test("Select address - title", async () => {
+
+    const {getByText} = render( <BrowserRouter><Provider store={store}> <MainPage></MainPage></Provider></BrowserRouter> );
+    const text = getByText("Lastly,");
+    expect(text).toBeInTheDocument();
+
+});
+
+test("Select address - body", async () => {
+
+    const {getByText} = render( <BrowserRouter><Provider store={store}> <MainPage></MainPage></Provider></BrowserRouter> );
+    const text = getByText("All of your personal data will be contained inside your pod(?)." +
+        " You can choose your adress and proceed with the");
     expect(text).toBeInTheDocument();
 
 });
