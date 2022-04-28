265212f6e7410fe9d5ddeb75e9ce741f468cc768
Martin Beltran <uo276244@uniovi.es>
2022-04-28 13:44:55 +0200

    Hotfix little test

--- webapp/src/components/Cart/Cart.tsx
+++ webapp/src/components/Cart/Cart.tsx
@@ -76,7 +76,7 @@ function Cart(props: CartProps): JSX.Element {
               Total:
               <b>{Utils.calculateTotal(products, shippingCost).toFixed(2)} €</b>
             </div>
-            <div title={"total"} className={styles.totalPrice}>
+            <div title={"estimDeliverDate"} className={styles.totalPrice}>
               Estimated delivery date:
               <b>{getEstimatedDeliveryDate()}</b>
             </div>
